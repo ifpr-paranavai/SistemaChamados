23ffe557718b3f3997651f0bc7402144d4ae29f6
Flag-fix <isac.bandeir4@gmail.com>
2022-06-01 18:21:08 -0300

    criação develop

--- api-sistemachamados/src/main/java/com/api/sistemachamados/controller/TesteController.java
+++ api-sistemachamados/src/main/java/com/api/sistemachamados/controller/TesteController.java
@@ -2,15 +2,17 @@ package com.api.sistemachamados.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/v1/test")
+@PreAuthorize("hasPermission('null', {'ROLE_USER', 'PERM_PROCURACAO'})")
 public class TesteController {
 
-    @PreAuthorize("hasPermission('null', {'ROLE_USER', 'PERM_PROCURACAO'})")
     @GetMapping
-    public ResponseEntity<String> auth(){
+    public ResponseEntity<String> auth() {
         System.out.println("Teste.");
         return ResponseEntity.ok("Testado");
     }
